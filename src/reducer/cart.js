a678f186a1b6b31aa17211e40f6ed2bd2270f55b
Julio Ucharima <julio_ucharima@usmp.pe>
2023-12-14 19:43:08 -0500

    feat: add local persistence to shopping cart

--- src/reducer/cart.js
+++ src/reducer/cart.js
@@ -1,4 +1,9 @@
-export const cartInitialState = [];
+export const cartInitialState =
+  JSON.parse(window.localStorage.getItem('cart')) || [];
+
+export const updateLocalStorage = (state) => {
+  window.localStorage.setItem('cart', JSON.stringify(state));
+};
 
 export const cartReducer = (state, action) => {
   const { type: actionType, payload: actionPayload } = action;
@@ -11,24 +16,35 @@ export const cartReducer = (state, action) => {
       if (productInCartIndex >= 0) {
         const newState = structuredClone(state);
         newState[productInCartIndex].quantity += 1;
+
+        updateLocalStorage(newState);
         return newState;
       }
 
-      return [
+      const newState = [
         ...state,
         {
           ...actionPayload,
           quantity: 1,
         },
       ];
+
+      updateLocalStorage(newState);
+
+      return newState;
     }
 
     case 'REMOVE_FROM_CART': {
       const { id } = actionPayload;
-      return state.filter((item) => item.id !== id);
+      const newState = state.filter((item) => item.id !== id);
+
+      updateLocalStorage(newState);
+
+      return newState;
     }
 
     case 'CLEAR_CART': {
+      updateLocalStorage(cartInitialState);
       return cartInitialState;
     }
   }
